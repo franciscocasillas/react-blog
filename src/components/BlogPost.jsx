dbb0c414ae968f3f32cb5518d256e7973cb1a639
Francisco J. Casillas <chiccasillas@gmail.com>
2024-05-24 12:06:35 -0600

    Create posts.js array with posts as objects

--- src/components/BlogPost.jsx
+++ src/components/BlogPost.jsx
@@ -2,13 +2,13 @@ import PropTypes from "prop-types";
 
 function BlogPost({ title, author, date, excerpt, image, post = true }) {
 	return (
-		<div>
+		<article>
 			<h2>{title}</h2>
 			<p>{author}</p>
 			<p>{date}</p>
 			{excerpt && <p>{excerpt}</p>}
 			{image && <img src={image} alt={title} />}
-		</div>
+		</article>
 	);
 }
 
---
+++ src/data/posts.js
@@ -0,0 +1,19 @@
+export const posts = [
+	{
+		title: "My First Post",
+		author: "Me",
+		date: "Today",
+		excerpt: "Something very cool",
+	},
+	{
+		title: "My Second Post",
+		author: "Me",
+		date: "Yesterday",
+	},
+	{
+		title: "My Third Post",
+		author: "Me",
+		date: "The day before yesterday",
+		excerpt: "Something extremely freshs",
+	},
+];
