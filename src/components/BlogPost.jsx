b7c163c128dd13f321f753ca159a94881ed9d2a4
Francisco J. Casillas <chiccasillas@gmail.com>
2024-05-24 11:47:52 -0600

    Create BlogPost component

---
+++ src/components/BlogPost.jsx
@@ -0,0 +1,24 @@
+import PropTypes from "prop-types";
+
+function BlogPost({ title, author, date, excerpt, image, post = true }) {
+	return (
+		<div>
+			<h2>{title}</h2>
+			<p>{author}</p>
+			<p>{date}</p>
+			{excerpt && <p>{excerpt}</p>}
+			{image && <img src={image} alt={title} />}
+		</div>
+	);
+}
+
+BlogPost.propTypes = {
+	title: PropTypes.string.isRequired,
+	author: PropTypes.string.isRequired,
+	date: PropTypes.string.isRequired,
+	excerpt: PropTypes.string,
+	image: PropTypes.string,
+	post: PropTypes.bool,
+};
+
+export default BlogPost;
